aaee4773b025d70dfc6adaa583b7d8fba4e0a877
Vasjan Çupri <vasjancupri04@gmail.com>
2023-12-14 20:43:51 +0100

    trying to fix auth

---
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,69 @@
+import prisma from "@/lib/prisma"
+import { compare } from "bcrypt"
+import { User, type NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+      CredentialsProvider({
+        name: "Credentials",
+        credentials: {
+          email: { label: "Email", type: "text", placeholder: "email" },
+          password: {
+            label: "Password",
+            type: "password",
+            placeholder: "password",
+          },
+        },
+        async authorize(credentials) {
+          if (!credentials?.email || !credentials?.password) return null
+          const user = await prisma.user.findUnique({
+            where: {
+              email: credentials.email,
+            },
+          })
+          if (!user) return null
+  
+          const isPasswordValid = await compare(
+            credentials.password,
+            user.password
+          )
+          if (!isPasswordValid) return null
+          return {
+            id: `${user.id}`,
+            name: user.name,
+            email: user.email,
+            username: user.username,
+          }
+        },
+      }),
+    ],
+    pages: {
+      signIn: "/login",
+    },
+    callbacks: {
+      session: ({ session, token }) => {
+        return {
+          ...session,
+          user: {
+            ...session.user,
+            id: token.id,
+            name: token.name,
+            username: token.username,
+          },
+        }
+      },
+      jwt({ token, user }) {
+        if (user) {
+          const u = user as unknown as any
+          return {
+            ...token,
+            id: u.id,
+            name: u.name,
+            username: u.username,
+          }
+        }
+        return token
+      },
+    },
+  }
